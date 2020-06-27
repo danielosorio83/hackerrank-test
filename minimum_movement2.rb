b5ae122c9eb62c7495c95c73254b44014c6cc8a5
Daniel Osorio <danielosorio83@gmail.com>
2020-06-27 12:47:31 -0500

    rubocop to minimum_movement2.rb

--- minimum_movement2.rb
+++ minimum_movement2.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 class MinimumMovementService
   LEFT_LANE = 1
   MIDDLE_LANE = 2
   RIGHT_LANE = 3
 
-  def initialize(obstacleLanes, starting_lane = MIDDLE_LANE)
-    @obstacleLanes = obstacleLanes
+  def initialize(obstacle_lanes, starting_lane = MIDDLE_LANE)
+    @obstacle_lanes = obstacle_lanes
     @car_lane = starting_lane
   end
 
   def call
     movements = 0
-    @obstacleLanes.each_with_index do |obstacle, i|
-      if obstacle == @car_lane
-        movements += 1
-        next_obstacles = @obstacleLanes.slice(i+1..-1)
-        break unless next_obstacles.include?(obstacle)
+    @obstacle_lanes.each_with_index do |obstacle, i|
+      next unless obstacle == @car_lane
 
-        if obstacle == RIGHT_LANE || obstacle == LEFT_LANE
-          @car_lane = MIDDLE_LANE
-        end
-      end
+      movements += 1
+      next_obstacles = @obstacle_lanes.slice(i + 1..-1)
+      break unless next_obstacles.include?(obstacle)
+
+      @car_lane = MIDDLE_LANE if [RIGHT_LANE, LEFT_LANE].include?(obstacle)
     end
     movements
   end
