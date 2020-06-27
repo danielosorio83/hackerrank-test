3e0d24aacd7e717e0d2f6c07daa4f5193c50e655
Daniel Osorio <danielosorio83@gmail.com>
2020-06-27 12:15:43 -0500

    time requests ruby code

---
+++ time_requests.rb
@@ -0,0 +1,32 @@
+class TimeRequestsService
+  DATETIME_FORMAT = /\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}/
+
+  def initialize(filename)
+    @filename = filename
+    @datetimes = {}
+  end
+
+  def call
+    datetimes = pull_datetimes_from_file
+    generate_req_file(datetimes)
+  end
+
+  def pull_datetimes_from_file
+    file = File.open(@filename)
+    file.readlines.each do |line|
+      if datetime = line.match(DATETIME_FORMAT)[0]
+        @datetimes[datetime] ||= 0
+        @datetimes[datetime] += 1
+      end
+    end
+  end
+
+  def generate_req_file(datetimes)
+    output = File.open("req_#{@filename}", 'w+')
+    @datetimes.each do |datetime, count|
+      next if count == 1
+      output.print("#{datetime}\n")
+    end
+    output
+  end
+end
